407edad1ca30e1ad9ccff93a2fee2a27181198d1
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2022-11-11 16:46:18 -0800

    Updated Header

--- website/src/components/Header.js
+++ website/src/components/Header.js
@@ -2,6 +2,9 @@ const Header = () => {
   return (
     <header>
         <h1>RTTM</h1>
+        <h2>Home</h2>
+        <h2>Technologies</h2>
+        <h2>Sign in/ Sign Up</h2>
     </header>
   )
 }
