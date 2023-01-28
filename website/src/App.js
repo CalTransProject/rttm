d2c997e890696134abda6d9021664971c31bb248
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2023-01-28 13:48:31 -0800

    added "pages" feature
    Installed the feature to be able to add different amount of pages

--- website/src/App.js
+++ website/src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPage from "./components/Mainpage";
 import MainPicture from "./components/MainPicture";
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
