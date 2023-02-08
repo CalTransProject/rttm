ac077407d126cb50d65b3b6825d795f7d794d37d
RobinRosculete <robin.rosculete.860@my.csun.edu>
2023-02-07 19:44:07 -0800

    Added subcomponent for images used in Technologies page

---
+++ website/src/components/subcomponents/TechnologyLidarPicture.js
@@ -0,0 +1,13 @@
+const TechnologyLidarPicture = () => {
+  return (
+    <div>
+      <img
+        src="./images/TechnologyLidarPicture.png"
+        alt=""
+        class="img-thumbnail"
+      />
+    </div>
+  );
+};
+
+export default TechnologyLidarPicture;
---
+++ website/src/components/subcomponents/TechnologyTrafficPicture.js
@@ -0,0 +1,13 @@
+const TechnologyTrafficPicture = () => {
+  return (
+    <div>
+      <img
+        src="./images/TechnologyTrafficPicture.png"
+        alt=""
+        class="img-thumbnail"
+      />
+    </div>
+  );
+};
+
+export default TechnologyTrafficPicture;
