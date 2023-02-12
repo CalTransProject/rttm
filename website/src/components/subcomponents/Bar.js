b11717485001df00901f31cd9268f6de34feab33
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2023-02-12 15:18:41 -0800

    Updated bar graph

--- website/src/components/subcomponents/Bar.js
+++ website/src/components/subcomponents/Bar.js
@@ -2,17 +2,44 @@ import ReactEcharts from "echarts-for-react";
 
 const StackedBar = () =>{
    const option = {
+    title:{
+      text:'Speed'
+    },
+    tooltip: {},
+    legend:{
+      data: ['']
+    },
     xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      name: 'Hour',
+      nameTextStyle:{
+        fontSize:12,
+        fontWeight:'bold'
+    },
+      data: ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', 
+      '8:00', '9:00', '10:00', '11:00', '12:00','13:00','14:00', '15:00', '16:00', 
+      '17:00','18:00', '19:00', '20:00', '21:00' , '22:00', '23:00']
     },
     yAxis: {
-      type: 'value'
+      name: 'Average Speed of Vehicles',
+      nameLocation:'middle',
+      nameGap:30,
+      nameTextStyle:{
+          fontSize:18,
+          fontWeight:'bold'
+      },
+      nameRotate:90,
+      data:['20', '40', '60', '80','100']
     },
     series: [
       {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
+        name: ' ',
+        type: 'bar',
+        data: []
+      },
+      {
+        name: ' ',
+        type: 'line',
+        data: []
       }
     ]
   };
