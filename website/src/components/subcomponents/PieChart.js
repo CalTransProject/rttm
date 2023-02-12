d309c24a14420bc30d89906476be4ebe1bea0d1c
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2023-02-12 15:25:22 -0800

    Update PieChart.js

--- website/src/components/subcomponents/PieChart.js
+++ website/src/components/subcomponents/PieChart.js
@@ -3,8 +3,7 @@ import ReactEcharts from "echarts-for-react";
 const PieChart = () =>{
    const option = {
     title: {
-      text: 'Referer of a Website',
-      subtext: 'Fake Data',
+      text: 'Percentage of vehicles',
       left: 'center'
     },
     tooltip: {
@@ -16,23 +15,15 @@ const PieChart = () =>{
     },
     series: [
       {
-        name: 'Access From',
         type: 'pie',
         radius: '50%',
         data: [
-          { value: 1048, name: 'Search Engine' },
-          { value: 735, name: 'Direct' },
-          { value: 580, name: 'Email' },
-          { value: 484, name: 'Union Ads' },
-          { value: 300, name: 'Video Ads' }
+          { value: '', name: 'SUV' },
+          { value: '', name: 'Sedan' },
+          { value: '', name: 'Car' },
+          { value: '', name: 'Truck' },
+          { value: '', name: 'Bus' }
         ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
       }
     ]
   };
