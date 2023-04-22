ed8b94c63553c53488dbac804c452eb758886328
RobinRosculete <robin.rosculete.860@my.csun.edu>
2023-04-22 13:50:33 -0700

    Updatad General Camera Management Page Pop up menu design

--- website/src/components/subcomponents/sub-camera-management/Modal.js
+++ website/src/components/subcomponents/sub-camera-management/Modal.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import "./styling/Modal.css";
+import { Wrench } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
 const ModalComponent = ({ modalInfo, showModal, handleCloseModal }) => {
   return (
-    <Modal show={showModal} onHide={handleCloseModal}>
+    <Modal show={showModal} onHide={handleCloseModal} size="lg">
       <Modal.Header closeButton>
-        <Modal.Title>Selected Row Data</Modal.Title>
+        <Modal.Title>Index: Camera {modalInfo.Index} </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         {modalInfo && (
           <div>
             <p>
-              <strong>Index: </strong>
-              {modalInfo.Index}
-            </p>
-            <p>
               <strong>Name: </strong>
               {modalInfo.Name}
+            </p>
+            <p>
+              <strong>Model: </strong>
+              {modalInfo.Camera_Model}
             </p>
             <p>
               <strong>Type: </strong>
@@ -30,11 +33,36 @@ const ModalComponent = ({ modalInfo, showModal, handleCloseModal }) => {
               <strong>Default: </strong>
               {modalInfo.Default}
             </p>
+            <p>
+              <strong>Description: </strong>
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows="5"
+                cols="55"
+                readOnly
+              >
+                {modalInfo.Camera_Description}
+              </textarea>
+            </p>
           </div>
         )}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleCloseModal}>
+        <Link to="/camera-management/configuration">
+          <Button
+            variant="secondary"
+            onClick={handleCloseModal}
+            className="configuration-button"
+          >
+            Configure <Wrench size={30} />
+          </Button>
+        </Link>
+        <Button
+          variant="secondary"
+          onClick={handleCloseModal}
+          className="close-button"
+        >
           Close
         </Button>
       </Modal.Footer>
