4f647bd3eb848c75a76b1bcaec1e2403d83bf5b2
ItsBrianUribe <brianuribe013@gmail.com>
2023-05-02 09:17:40 -0700

    Upload Capabilities Added
    Added the capability to upload files to an AWS bucket with the proper credentials from the website itself.

--- website/src/components/historical-data-subpages/HistoricalUpload.js
+++ website/src/components/historical-data-subpages/HistoricalUpload.js
@@ -1,12 +1,53 @@
-import './styling/general.css';
 import React, { useState } from 'react';
+import AWS from 'aws-sdk';
+import './styling/upload.css'
 
-const HistoricalUpload = () => {
+AWS.config.update({
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: 'us-west-1' // Change this to your S3 bucket region
+});
+
+const s3 = new AWS.S3();
+
+const awsconnect = (file) => {
+  const params = {
+    Bucket: 'updownbucket',
+    Key: file.name,
+    Body: file
+  };
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log('Error uploading file:', err);
+    } else {
+      console.log('File uploaded successfully:', data.Location);
+    }
+  });
+};
+
+function HistoricalUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileUpload = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (selectedFile) {
+      awsconnect(selectedFile);
+    }
+  };
+
   return (
-    <div className='GeneralSection'>
-      <h1>Upload</h1>
+    <div className='upload'>
+        <h2>Upload Data</h2>
+        <form className='fileUpload' onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileUpload} />
+            <button type="submit">Upload</button>
+        </form>
     </div>
   );
-};
+}
 
 export default HistoricalUpload;
