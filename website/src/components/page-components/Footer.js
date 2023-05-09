7881d95844bfd8de77a8ec7a8ef32e6a7fc597c5
ItsBrianUribe <brianuribe013@gmail.com>
2023-05-09 13:31:04 -0700

    Revert "Joses footer and about us"
    This reverts commit 77b6e25614444d7140d808567bba229104c8eb1e.

--- website/src/components/page-components/Footer.js
+++ website/src/components/page-components/Footer.js
@@ -4,27 +4,26 @@ import "./footer.css";
 const Footer = () => { 
     return (
         <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-light text-white-50">
-            <div className="footer-all">
+            <div class="container text-center">
                 <small>
-                    <a href="/home" className="footer-links" 
-                        class="footer-link">Home
+                    <a className="footer-links" href="/">
+                        <button type="button" class="btn btn-link">Home</button>
                     </a>
-                    <a className="footer-links" href="/camera-management"
-                         class="footer-link">Camera Management
+                    <a className="footer-links" href="/camera-management/general">
+                        <button type="button" class="btn btn-link">Camera Management</button>
                     </a>
-                    <a className="footer-links" href="/historical-data"
-                         class="footer-link">Historical Data
+                    <a className="footer-links" href="/historical-data/general">
+                        <button type="button" class="btn btn-link">Historical Data</button>
                     </a>
-                    <a className="footer-links" href="/technologies"
-                         class="footer-link">Technologies
+                    <a className="footer-links" href="/technologies">
+                        <button type="button" class="btn btn-link">Technologies</button>
                     </a>
-                    <a className="footer-links" href="/about-us"
-                       class="footer-link">About Us
+                    <a className="footer-links" href="/about-us">
+                        <button type="button" class="btn btn-link">About Us</button>
                     </a>
                     
                 </small>
             </div>
-            <div class="footer-line"></div>
         </footer>
     )
 } 
