7eacf044bdf49d25ab56c824257e979ccb3441aa
Jimwell Castillo <castvier@gmail.com>
2023-10-24 11:31:32 -0700

    Fixed runtime errors that spans all pages

--- website/src/components/subcomponents/sub-s3-components/api.js
+++ website/src/components/subcomponents/sub-s3-components/api.js
@@ -1,9 +1,31 @@
 export const fetchStreamInfo = async () => {
-  const arccsunMetaApi = 'https://2fc91z5147.execute-api.us-east-1.amazonaws.com/arcscsun/app'
-  return await fetch(arccsunMetaApi).then(async (res) => res.status === 200 ? await res.json() : {})
-}
+  try {
+    const arccsunMetaApi = 'https://2fc91z5147.execute-api.us-east-1.amazonaws.com/arcscsun/app';
+    const response = await fetch(arccsunMetaApi);
+    if (response.status === 200) {
+      return await response.json();
+    } else {
+      console.error(`Failed with status: ${response.status}`);
+      return {};
+    }
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+    return {};
+  }
+};
 
 export const fetchDataset = async (sid) => {
-  const arccsunApi = `https://kyld5svbf5.execute-api.us-east-1.amazonaws.com/arcscsun/app?sid=${sid}`
-  return await fetch(arccsunApi).then(async (res) => res.status === 200 ? await res.json() : {})
-}
+  try {
+    const arccsunApi = `https://kyld5svbf5.execute-api.us-east-1.amazonaws.com/arcscsun/app?sid=${sid}`;
+    const response = await fetch(arccsunApi);
+    if (response.status === 200) {
+      return await response.json();
+    } else {
+      console.error(`Failed with status: ${response.status}`);
+      return {};
+    }
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+    return {};
+  }
+};
