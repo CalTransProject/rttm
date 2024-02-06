7e003d07b4a0b32e0b370a9a5d7991d6b12b114c
Raymond <113494325+RaySD03@users.noreply.github.com>
2024-02-05 18:27:58 -0800

    Added firebase.js
    Config file for Firebase.

---
+++ website/src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs needed
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that are to be used
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7U5BGazjmJ-JhKcXawyQ7pYDvIKfR3lA",
+  authDomain: "rttm-authentication.firebaseapp.com",
+  projectId: "rttm-authentication",
+  storageBucket: "rttm-authentication.appspot.com",
+  messagingSenderId: "422990141948",
+  appId: "1:422990141948:web:6eefba0160d2159daf3a79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app
