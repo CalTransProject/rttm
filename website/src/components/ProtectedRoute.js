d51f0596b101ae915f87f96627e8d0e9a93e9913
Raymond Babayans <113494325+RaySD03@users.noreply.github.com>
2024-03-05 10:13:06 -0800

    Updated ProtectedRoute.js
    Works with user role defined in the database.

--- website/src/components/ProtectedRoute.js
+++ website/src/components/ProtectedRoute.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+import Header from './page-components/Header';
+import Mainpage from './Mainpage';
 
 const ProtectedRoute = ({children}) => {
+
+    const navigate = useNavigate();
+    const [permission, setPermission] = useState('');
     const { user } = UserAuth();
 
-    if (!user) {
-        return <Navigate to='/user-authentication' />;
+    const getPermission = async () => {
+        const QuerySnapshot = await getDoc(doc(db, "user_list", auth.currentUser.uid));
+        setPermission(QuerySnapshot.data()['role']);
     }
+    
+    getPermission()
+
+    if (!user) {
+        if (/my-account/.test(window.location.href) == false)
+        {
+            alert("You must be logged in to access this page.")           
+        }     
+        return <Navigate to='/user-authentication' />
+    }
+
+    if (permission == "read-only" && /my-account/.test(window.location.href) == false) {
+        alert("Admin access only")       
+        navigate('/my-account') 
+    }
+
     return children;
 };
 
