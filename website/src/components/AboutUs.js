60f9ad573d3ed70a42db9d68144ba7d3cbe1c86e
Jimwell Castillo <castvier@gmail.com>
2024-03-28 07:54:33 -0700

    Updated About Us page

--- website/src/components/AboutUs.js
+++ website/src/components/AboutUs.js
@@ -1,111 +1,63 @@
 import React from 'react';
 import "./subcomponents/sub-AboutUs/AboutUs.css"
 
-const AboutUs = () => { 
-    return(
-        <section>
-          <div className="box">
-                  <h1 className="about-title">About Us</h1>
-                  
-                  <div className="main-content">
-                  <h2>Our Project</h2>  
-                      <p className='aboutUsP'>Vehicle detection plays an important role in analyzing traffic flow data for efficient planning in intelligent transportation. Machine Learning technology has been increasingly used for vehicle detection in both 2D real-time traffic flow video and 3D point clouds. Adverse weather conditions, such as fog, rain, snow, extreme wind, and other conditions prove to be challenging for 2D vehicle detection. 3D LiDAR point clouds can be more resistant to adverse weather conditions. Most of the existing research on 3D vehicle detection are used for autonomous vehicle driving with the LiDAR cameras are deployed on vehicles. There is a lack of research on real-time vehicle detection for intelligent transportation with LiDAR cameras deployed by highway/freeway. In this project, we propose to build a system that collects real-time traffic flow data through 3D LiDAR cameras, processes the 3D point cloud data for vehicle detection and classification, and provides a web-based service with vehicles detected and classified in real-time traffic flow and data visualization for statistical traffic flow data.</p>
-                  </div>
-                  <h2>Our team</h2>
-                  
-                                  
-                  <div class="container">
-                    <div class="membersDisplay">  
-                      <div class="member">
-                        <div class="name">Andres Mercado</div>
-                        <img src="/images/img-AboutUs/Andres.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">I am a graduating Senior, interested in Robotics and Machine Learning. On this project I helped the Machine Learning team integrate the tiny Yolo algorithm that was the best fit for our project. </div>
-                        <a href="https://github.com/andymerc">
-                        <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      
-                      <div class="member">
-                        <div class="name">Alondra Gonzalez</div>
-                        <img src="/images/img-AboutUs/alondra_resize.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hello my name's Alondra Gonzalez, I worked on the data acquisition process and configuring the AWS Kinesis Video Streams module to add a stream to the project website.</div>
-                        <a href="https://github.com/AlondraGonzalez211">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Alexander Rose</div>
-                        <img src="/images/img-AboutUs/rose_alexander.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Graduate student - M.S. Software Engineering at CSUN. I helped build the real-time system on AWS</div>
-                        <a href="https://github.com/rosealexander">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Brian Uribe</div>
-                        <img src="/images/img-AboutUs/brian_resize.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hello! My name is Brian and I am a senior pursuing a bachelor's in computer science. I helped work on the backend of the RTTM website and was the one behind the video players that display the real-time video feed coming from the cameras connected to our system.</div>
-                        <a href="https://github.com/ItsBrianUribe">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Icess Nisce</div>
-                        <img src="/images/img-AboutUs/icess_resize.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hi, I'm Icess. I'm an undegraduate senior majoring in Computer Science, with interests in machine learning, AI, AR/VR, web development and design. My role in the Real-Time Traffic Monitoring Project involves the development of the 3D vehicle detection model and real-time streaming capabilities of the system.</div>
-                        <a href="https://github.com/sseci">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      
-                      <div class="member">
-                        <div class="name">Matthew Davis</div>
-                        <img src="/images/img-AboutUs/matthew_resize.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">My name is Matthew Davis. I am a 22 year old CSUN student working towards a bachelor’s in Computer Science. My work within the Real-Time Traffic Monitoring group has solely revolved around designing the model for 2D object detection.</div>
-                        <a href="https://github.com/CalTransProject">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Javier Carranza</div>
-                        <img src="/images/img-AboutUs/javier3_resize.jpg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hello, I'm Javier pursuing a bachelor's in computer science. My main role in the project is to help make progress on the RTTM website and was the one behind the charts that display the real-time data coming from the cameras connected to our system.</div>
-                        <a href="https://github.com/JavierACarranza">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Robin Rosculete</div>
-                        <img src="/images/img-AboutUs/Robin.png" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hello, My name is Robin. Im a CS undegrad student. And I worked on Web Development and Machine Learning</div>
-                        <a href="https://github.com/RobinRosculete">
-                        <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                      <div class="member">
-                        <div class="name">Jose Flores</div>
-                        <img src="/images/img-AboutUs/Jose.jpeg" alt="Student pfp" class="pfp-photo"></img>
-                        <br></br>
-                        <div class="description">Hello! Im a undergrad student majoring in computer science. In this project I focused on labeling LiDar data and Web Development  </div>
-                        <a href="https://github.com/Jose-Flor">
-                          <img src="/images/img-AboutUs/githublogo.png" alt="Git hub logo" class="github-photo"></img>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-    
-        <img src="/images/img-AboutUs/CSUNlogo.png" alt="Image at the bottom"></img>
-     
+const AboutUs = () => {
+  return (
+    <section>
+      <div className="box">
+        <h1 className="about-title">About Us</h1>
+        <div className="main-content">
+          <h2>Our Project Introduction</h2>
+          <p className='aboutUsP'>
+            The primary aim of our project is to enhance the efficiency of intelligent transportation planning through advanced vehicle detection techniques. Leveraging the robustness of 3D LiDAR technology, we endeavor to overcome the limitations faced by traditional 2D vehicle detection methods under adverse weather conditions like fog, rain, snow, and extreme winds. Our project introduces an innovative system designed to capture, process, and analyze real-time traffic flow data using 3D LiDAR cameras strategically deployed along highways and freeways, areas where existing research on 3D vehicle detection, typically focused on autonomous driving, falls short.
+            Our system stands out by offering a comprehensive web-based service that not only detects and classifies vehicles in real-time traffic flows but also presents this data through dynamic, user-friendly visualizations. This approach facilitates a deeper understanding of traffic patterns, supporting more informed decisions in traffic management and planning. By bridging the gap in current research and applying 3D LiDAR technology in a novel context, our project sets a new standard for real-time vehicle detection and traffic data analysis, promising significant improvements in the field of intelligent transportation systems.
+          </p>
+        </div>
+        <h2>Our team</h2>
+        <div class="container">
+          <div class="membersDisplay">
+            <div class="member">
+              <div class="name">Jimwell Castillo</div>
+              <br></br>
+              <div class="description">As our leader and front-end developer, Jimwell has masterfully crafted the homepage, showcasing his adeptness in UX/UI design. His vision and execution have provided a user-friendly gateway to our traffic monitoring system, ensuring ease of access and engagement for all users. He also worked on the database to bridge historical data visualization.</div>
+              <a href="https://github.com/castvier">https://github.com/castvier</a>
             </div>
-        </section>
-    )
-}
-export default AboutUs; 
+            <div class="member">
+              <div class="name">Kaylee Groves</div>
+              <br></br>
+              <div class="description">Kaylee spearheaded the development of our camera management feature. Her expertise in front-end development, specifically within React Native, has been crucial in implementing functionalities that enable efficient camera control and data acquisition, enhancing our system's capability to monitor traffic in real-time.</div>
+              <a href="https://github.com/kaygroves">https://github.com/kaygroves</a>
+            </div>
+            <div class="member">
+              <div class="name">Raymond Gharapeti Babayans</div>
+              <br></br>
+              <div class="description">Raymond has been pivotal in implementing user management through Google Firebase. His efforts ensure a seamless, secure, and efficient user experience, from account creation and login to data privacy and access control, emphasizing the importance of user data protection and personalized user interaction.</div>
+              <a href="https://github.com/RaySD03">https://github.com/RaySD03</a>
+            </div>
+            <div class="member">
+              <div class="name">Gisela Calva</div>
+              <br></br>
+              <div class="description">Gisela initiated the development of historical data visualization, utilizing her skills in data analysis and visualization to transform raw traffic data into insightful, accessible visual narratives. This foundation has been instrumental in allowing users to understand and interpret traffic trends over time.</div>
+              <a href="https://github.com/Giselv">https://github.com/Giselv</a>
+            </div>
+            <div class="member">
+              <div class="name">Anthony Merzoian</div>
+              <br></br>
+              <div class="description">Focused on enhancing our system's security and developing the notification feature, Anthony has ensured that our platform is not only secure against potential threats but also capable of providing timely alerts to users. His work underscores our commitment to delivering a reliable and safe user experience.</div>
+              <a href="https://github.com/anthonymerzoian">https://github.com/anthonymerzoian</a>
+            </div>
+            <div class="member">
+              <div class="name">Alma Dollente</div>
+              <br></br>
+              <div class="description">Alma's role in planning and managing the historical data component of our project lays the groundwork for future enhancements in how traffic data is stored, processed, and analyzed. Her strategic approach to data management planning ensures our system's scalability and the integrity of long-term data analysis.</div>
+              <a href="https://github.com/No0d1e">https://github.com/No0d1e</a>
+            </div>
+          </div>
+        </div>
+        <div className="spacing-below-team"></div> {/* Extra spacing div */}
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
