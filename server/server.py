209c0c6ee252c599db44b9704d5c8292c96d5b71
RobinRosculete <robin.rosculete.860@my.csun.edu>
2024-05-01 23:44:37 -0700

    Added Api  endpoint to for prediciton on video and also modularized the webcam code

--- server/server.py
+++ server/server.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, Response
+import tempfile
+from flask import Flask,  Response, request, jsonify
+import cv2
+from ultralytics import YOLO
 from webcam import gen_frames
+import os
+
 
 
 # Initializing flask app
@@ -9,6 +14,70 @@ app = Flask(__name__)
 @app.route('/webcam')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/upload-and-predict', methods=['POST'])
+def upload_and_predict():
+    # Loading the 2d Model 
+    PATH_TO_MODEL = "../model/2dModel.pt"
+    model = YOLO(PATH_TO_MODEL)  # Assuming YOLO class is defined
+    
+    #Geeting the file from the http request
+    file = request.files['file']
+    
+    #Creating a Temporary video file to store file
+    filename = 'uploaded_video.mp4'
+    file_path = os.path.join('./video-uploads', filename)
+    
+    #Saving the gile in the new directory
+    file.save(file_path)
+    
+    # Loading the video with opencv 
+    cap = cv2.VideoCapture(file_path)
+    
+    # Getting the video frame width and height
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    # Define the codec and create VideoWriter object
+
+    #Defining the codec to creat VidewWriter  object
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret: 
+            break
+        
+        # Yolo V8 detection 
+        results = model(frame)
+        print(results)
+        cv2.waitKey(1)
+        
+        # Plotting the results and saving the video 
+        res_plotted = results[0].plot()
+        out.write(res_plotted)
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+        # Process detection results
+        for detection_result in results:
+            boxes = detection_result.boxes  # Boxes object for bounding box outputs
+            clss = boxes.cls  # Detected Class 
+            xyxy = boxes.xyxy
+            xywh = boxes.xywh
+            print("Class:", clss)
+    
+ 
+               
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()     
+      
+    return 'Successfully Proccesed video'
+
 
 # Running app
 if __name__ == '__main__':
--- server/webcam.py
+++ server/webcam.py
@@ -3,10 +3,6 @@ from datetime import datetime
 from ultralytics import YOLO
 import math
 
-camera = cv2.VideoCapture(0)
-
-PATH_TO_MODEL = "/Users/Robin1/Desktop/LIDAR Project/rttm/model/2dModel.pt"
-model = YOLO(PATH_TO_MODEL)
 
 # Object classes
 classNames = [
@@ -23,53 +19,39 @@ classNames = [
 # Dictionary to store class counts
 class_counts = {cls_name: 0 for cls_name in classNames}
 
-def gen_frames():
+
+def process_frame(frame, model):
+    results = model(frame, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+            class_counts[classNames[cls]] += 1
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (0, 0, 0)
+            thickness = 2
+            cv2.putText(frame, classNames[cls] + "  " + str(confidence), org, font, fontScale, color, thickness)
+    return frame
+
+def gen_frames(video_source=0):
+    PATH_TO_MODEL = "../model/2dModel.pt"
+    model = YOLO(PATH_TO_MODEL)
+    camera = cv2.VideoCapture(video_source)
     while True:
-        success, frame = camera.read()  # read the camera frame
-        results = model(frame, stream=True)
-        # coordinates
-        for r in results:
-            boxes = r.boxes
-
-            for box in boxes:
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
-                
-                
-
-                # put box in cam
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                print("(x1, y1) =", (x1, y1), "(x2, y2) =", (x2, y2))
-
-                # confidence
-                confidence = math.ceil((box.conf[0] * 100)) / 100
-
-                # class name
-                cls = int(box.cls[0])
-
-                # Update class count
-                class_counts[classNames[cls]] += 1
-
-                # Object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (0, 0, 0)
-                thickness = 2
-                cv2.putText(frame, classNames[cls] + "  " + str(confidence), org, font, fontScale, color, thickness)
-
-        # Print date and time in the upper right corner
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cv2.putText(frame, current_time, (frame.shape[1] - 350, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-
+        success, frame = camera.read()
         if not success:
             break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
+        frame = process_frame(frame, model)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cv2.putText(frame, current_time, (frame.shape[1] - 350, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 1, cv2.LINE_AA)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera.release()
+    cv2.destroyAllWindows()
